aca33c8fc51042341836f376f8383d69a7e995d0
Eduardo <eduardo_felipe_oliveira@yahoo.com.br>
2017-12-12 17:37:21 -0200

    Correção da Promisse

--- index.js
+++ index.js
@@ -19,35 +19,40 @@ var config = {
 
 imaps.connect(config).then(function (connection) {
 
-    return connection.openBox('INBOX').then(function () {
+    connection.openBox('INBOX').then(function () {
         var searchCriteria = [
             'ALL',
             ['SUBJECT', 'BSS Brastel - Novo Pedido'],
             ['SINCE', 'Sep 30, 2017'],
             ['BEFORE', 'Nov 1, 2017']
-        ];
+        ]
 
         var fetchOptions = {
             bodies: ['HEADER', 'TEXT'],
             markSeen: false
-        };
+        }
 
-        return connection.search(searchCriteria, fetchOptions).then(function (results) {
+        return connection.search(searchCriteria, fetchOptions)
+    }).then(function (results) {
 
-            results.map(item => {
-                html = (new Buffer(item.parts[0].body, 'base64').toString('ascii'))
-                idPedido = html.substr((html.search(/OrderEdit.aspx\?id=/i) + 18), 4)
-                lista.push([dateFormat(item.parts[1].body.date[0], "dd-mm-yyyy HH:MM:ss"), idPedido, item.parts[1].body.subject[0]])
-                //console.log(`Data: ${dateFormat(item.parts[1].body.date[0], "dd-mm-yyyy HH:MM:ss")}\tId pedido: ${idPedido}\t\tTitulo: ${item.parts[1].body.subject[0]}`)
-            })
+        results.map(item => {
+            html = (new Buffer(item.parts[0].body, 'base64').toString('ascii'))
+            idPedido = html.substr((html.search(/OrderEdit.aspx\?id=/i) + 18), 4)
+            lista.push([dateFormat(item.parts[1].body.date[0], "dd-mm-yyyy HH:MM:ss"), idPedido, item.parts[1].body.subject[0]])
+        })
 
-            var file = fs.createWriteStream('Novo Pedido.csv');
-            file.on('error', function(err) { });
-            lista.forEach(function(v) {
-                file.write(v.join(',') + '\r\n');
-            });
-            file.end();
+        return lista
 
+    }).then(function (lista) {
+
+        var file = fs.createWriteStream('Novo Pedido.csv');
+        file.on('error', function (err) { });
+        lista.forEach(function (v) {
+            file.write(v.join(',') + '\r\n');
         });
-    });
-});
+        file.end();
+
+    }).then(() => {
+        process.exit()
+    })
+})
